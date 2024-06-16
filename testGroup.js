fcffa8bf4d6e9e6e2d1e6ab39f99ac164adcdf71
Duong Ngoc Phuong Anh <163420879+dngcphngnh@users.noreply.github.com>
2024-06-16 21:24:07 +0700

    Add files via upload

---
+++ testGroup.js
@@ -0,0 +1,119 @@
+
+d3.csv("https://raw.githubusercontent.com/dngcphngnh/DSDV/main/Data.csv").then(function(data) {
+    // Group and summarize data
+    let groupedData = Array.from(
+        d3.rollup(data, v => v.length, d => d.Age),
+        ([Age, Count]) => ({
+            Age: +Age,
+            Count
+        })
+    )
+
+    console.log(groupedData); // Check the transformed data structure
+
+    let width = 928;
+    let height = 600;
+    let marginTop = 40;
+    let marginRight = 20;
+    let marginBottom = 30;
+    let marginLeft = 30;
+
+    const x = d3.scaleBand()
+        .domain(groupedData.map(d => d.Age))
+        .range([marginLeft, width - marginRight])
+        .padding(0.1);
+
+        const xAxis = d3.axisBottom(x).tickSizeOuter(0);
+
+  
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(groupedData, d => d.Count)]).nice()
+        .range([height - marginBottom, marginTop]);
+
+        const yAxis = d3.axisLeft(y);
+
+    // Create the SVG container.
+    const svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .attr("style", "max-width: 100%; height: auto;");
+
+    // Add a rect for each bar.
+    svg.append("g")
+        .attr("fill", "steelblue")
+        .selectAll("rect")
+        .data(groupedData)
+        .join("rect")
+        .attr("x", d => x(d.Age))
+        .attr("y", d => y(d.Count))
+        .attr("height", d => y(0) - y(d.Count))
+        .attr("width", x.bandwidth())
+        .on("mouseover", function(event, d) {
+            d3.select(this).attr("fill", "#03045e");
+            tooltip.transition().duration(200).style("opacity", 1);
+            tooltip.html(`Age: ${d.Age}<br>Count: ${d.Count}`)
+                .style("left", (event.pageX + 5) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        })
+        .on("mouseout", function() {
+            d3.select(this).attr("fill", "steelblue");
+            tooltip.transition().duration(200).style("opacity", 0);
+        });
+
+    // Add the x-axis and label.
+    svg.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(d3.axisBottom(x).tickSizeOuter(0))
+        .append("text")
+        .attr("x", width - marginRight)
+        .attr("y", -4)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "end")
+        .text("Age");
+
+    // Add the y-axis and label, and remove the domain line.
+    svg.append("g")
+        .attr("transform", `translate(${marginLeft},0)`)
+        .call(d3.axisLeft(y))
+        .call(g => g.select(".domain").remove())
+        .call(g => g.selectAll(".tick line").clone()
+            .attr("x2", width - marginLeft - marginRight)
+            .attr("stroke-opacity", 0.1))
+        .append("text")
+        .attr("x", -marginLeft)
+        .attr("y", 10)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "start")
+        .text("↑ Count");
+
+    // Create a tooltip div that is hidden by default
+    const tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("position", "absolute")
+        .style("text-align", "center")
+        .style("width", "120px")
+        .style("height", "40px")
+        .style("padding", "2px")
+        .style("background", "white")
+        .style("border", "0px")
+        .style("border-radius", "8px")
+        .style("pointer-events", "none")
+        .style("opacity", 0);
+
+    function zoom(svg) {
+        const extent = [[marginLeft, marginTop], [width - marginRight, height - marginTop]];
+
+        svg.call(d3.zoom()
+            .scaleExtent([1, 8])
+            .translateExtent(extent)
+            .extent(extent)
+            .on("zoom", zoomed));
+
+        function zoomed(event) {
+            x.range([marginLeft, width - marginRight].map(d => event.transform.applyX(d)));
+            svg.selectAll(".bars rect").attr("x", d => x(d.Count)).attr("width", x.bandwidth());
+            svg.selectAll(".x-axis").call(xAxis);
+        }
+    }
+});
